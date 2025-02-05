5c728095e653f2b5b196460f254e1064036691b8
keba_e <dfsdfsd@fsdfsd.com>
2025-02-05 09:46:06 +0200

    2

--- main.py
+++ main.py
@@ -20,12 +20,37 @@ KEY_LEFT = "a"
 KEY_RIGHT = "d"
 
 matrix = []
-grid_cell = []
+grid_cells = []
 mainframe = Frame()
+
 
 def init_grid():
     background = Frame(bg=BACKGROUND_COLOR, width=SIZE, height=SIZE)
     background.grid()
+    for i in range(GRID_LEN):
+        grid_row = []
+        for j in range(GRID_LEN):
+            cell = Frame(background, bg=BACKGROUND_COLOR_CELL_EMPTY,
+                         width=SIZE / GRID_LEN, height=SIZE / GRID_LEN)
+            cell.grid(row=i, column=j, padx=GRID_PADDING, pady=GRID_PADDING)
+            t = Label(master=cell, text="", bg=BACKGROUND_COLOR_CELL_EMPTY,
+                      justify=CENTER, font=FONT, width=5, height=2)
+            t.grid()
+            grid_row.append(t)
+        grid_cells.append(grid_row)
+
+def init_matrix():
+    for i in range(GRID_LEN):
+        matrix.append([0] * GRID_LEN)
+def add_two():
+    a = random.randint(0, len(matrix)-1)
+    b = random.randint(0, len(matrix)-1)
+
+
+def main():
+    init_grid()
     mainloop()
 
-init_grid()
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,4 @@
+matrix = []
+for i in range(4):
+    matrix.append([0] * 4)
+print(matrix)
